a457c957bae12b170f68fa392f2ade6625bd0417
rafaelzochling <rafaelzochling@gmail.com>
2025-07-01 14:59:00 +0200

    first exercise of initiating and minting simple spl

---
+++ ts/cluster1/config.ts
@@ -0,0 +1,3 @@
+import wallet from '/Users/rafaelzochling/.config/solana/dev-turbin3-wallet.json'
+
+export const TURBIN3_WALLET = wallet
--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,19 +1,27 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import { Keypair, Connection, Commitment } from '@solana/web3.js'
+import { createMint } from '@solana/spl-token'
+import { TURBIN3_WALLET } from './config'
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = Keypair.fromSecretKey(new Uint8Array(TURBIN3_WALLET))
 
 //Create a Solana devnet connection
-const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const commitment: Commitment = 'confirmed'
+const connection = new Connection('https://api.devnet.solana.com', commitment)
 
-(async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
+;(async () => {
+  try {
+    // Start here
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6
+    )
+
+    console.log('Mint address:', mint)
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`)
+  }
 })()
